b3202a78fe076bbaf480453257d849808ddab954
pishifat <31551350+pishifat@users.noreply.github.com>
2019-04-16 19:10:30 -0700

    the most minor thing possible

--- models/bnTest/question.js
+++ models/bnTest/question.js
@@ -7,7 +7,7 @@ const questionSchema = new mongoose.Schema({
         'timing', 'audio', 'videoBackground', 'skinning', 
         'storyboarding', 'osu', 'taiko', 'catch', 'mania', 'bn'], required: true},
     content: { type: String, required: true },
-    active: { type: Boolean, default: true },
+    active: { type: Boolean, default: false },
     questionType: { type: String, enum: ["text", "image", "fill"], required: true },
     options: [{ type: 'ObjectId', ref: 'Option'}],
 }, { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } });
