2d446408ca917dc3cdee0cc04a71ec15c76379f9
pishifat <31551350+pishifat@users.noreply.github.com>
2019-05-16 01:39:16 -0700

    hide nat reports from feed

--- routes/reports.js
+++ routes/reports.js
@@ -35,26 +35,28 @@ router.post('/submitReport/', api.isLoggedIn, async (req, res) => {
         }
         await reportsService.create(req.session.mongoId, u.id, req.body.reason, req.body.link);
         res.json({});
-        api.webhookPost([{
-            author: {
-                name: `User report: ${u.username}`,
-                icon_url: `https://a.ppy.sh/${u.osuId}`,
-                url: `https://osu.ppy.sh/users/${u.osuId}`
-            },
-            color: '12184229',
-            fields:[
-                {
-                    name: `Report reason`,
-                    value: req.body.reason
-                }
-            ]
-        }]);
-        logsService.create(
-            null,
-            `Reported "${u.username}" for reason "${
-                req.body.reason.length > 50 ? req.body.reason.slice(0, 50) + '...' : req.body.reason
-            }"`
-        );
+        if(u.group != 'nat'){
+            api.webhookPost([{
+                author: {
+                    name: `User report: ${u.username}`,
+                    icon_url: `https://a.ppy.sh/${u.osuId}`,
+                    url: `https://osu.ppy.sh/users/${u.osuId}`
+                },
+                color: '12184229',
+                fields:[
+                    {
+                        name: `Report reason`,
+                        value: req.body.reason
+                    }
+                ]
+            }]);
+            logsService.create(
+                null,
+                `Reported "${u.username}" for reason "${
+                    req.body.reason.length > 50 ? req.body.reason.slice(0, 50) + '...' : req.body.reason
+                }"`
+            );
+        }
     }else{
         await reportsService.create(req.session.mongoId, null, req.body.reason, req.body.link);
         res.json({});
