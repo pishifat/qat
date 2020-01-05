542a1157ab0b9c2323e1cdcd3d2dfb975ad66125
venix12 <chopek.micha@gmail.com>
2020-01-05 05:15:25 +0100

    regex improvements

--- src/mixins/filterLinks.js
+++ src/mixins/filterLinks.js
@@ -2,18 +2,22 @@ const filterLinks = {
     methods: {
         filterLinks (text) {
             return (text || '...').replace(
-                /([^\S]|\[url\=|^)(((https?\:\/\/)|(www\.))(\S+))/gi,
-                function(match, space, url){
+                /([^\S]|\[url\=|\(|^)(((https?\:\/\/)|(www\.))(\S+))/gi,
+                (match, space, url) => {
                     let afterHyperlink = '';
                     let hyperlink = url;
                     let renderHyperlink = url;
 
-                    if(hyperlink.includes(']')) {
-                        const split = hyperlink.split(']');
-                        afterHyperlink = `]${split[1]}`;
-                        hyperlink = split[0];
-                        renderHyperlink = split[0];
-                    }
+                    const endings = [')', ']'];
+
+                    endings.forEach(ending => {
+                        if (hyperlink.includes(ending)) {
+                            const split = hyperlink.split(ending);
+                            afterHyperlink = `${ending}${split[1]}`;
+                            hyperlink = split[0];
+                            renderHyperlink = split[0];
+                        }
+                    });
 
                     if (!hyperlink.match(/^https?:\/\//)) {
                         hyperlink = `http://${hyperlink}`;
