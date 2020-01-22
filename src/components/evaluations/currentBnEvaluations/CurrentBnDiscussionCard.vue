7ea6c9d0a1aa09cdbbb7b72d3b0121c769049aae
pishifat <31551350+pishifat@users.noreply.github.com>
2020-01-22 00:28:57 -0800

    show consensus on currentbneval

--- src/components/evaluations/currentBnEvaluations/CurrentBnDiscussionCard.vue
+++ src/components/evaluations/currentBnEvaluations/CurrentBnDiscussionCard.vue
@@ -14,6 +14,7 @@
                 :username="evalRound.bn.username"
                 :osuId="evalRound.bn.osuId"
                 :consensus="evalRound.consensus"
+                :is-nat="true"
             />
             <card-footer
                 :mode="evalRound.mode"
