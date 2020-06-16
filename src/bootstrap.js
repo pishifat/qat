431173cb836bd43a8085db1d3e9e3e319246dfa5
milaneyia <39941577+milaneyia@users.noreply.github.com>
2020-06-15 20:16:10 -0400

    open links in new tabs

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -8,6 +8,23 @@ const md = new MarkdownIt('zero', {
     typographer: false,
 }).enable(['emphasis', 'linkify', 'newline']);
 
+// Remember old renderer, if overridden, or proxy to default renderer
+const defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+};
+
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    let aIndex = tokens[idx].attrIndex('target');
+
+    if (aIndex < 0) {
+        tokens[idx].attrPush(['target', '_blank']);
+    } else {
+        tokens[idx].attrs[aIndex][1] = '_blank';
+    }
+
+    return defaultRender(tokens, idx, options, env, self);
+};
+
 Vue.prototype.$md = md;
 
 $(document).ready(function() {
