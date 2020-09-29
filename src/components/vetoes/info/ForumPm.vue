355492ac148a3155a170a6f875ee1f9889f2403d
pishifat <pishifat@gmail.com>
2020-09-29 16:20:16 -0700

    fix veto pm

--- src/components/vetoes/info/ForumPm.vue
+++ src/components/vetoes/info/ForumPm.vue
@@ -1,13 +1,18 @@
 <template>
     <div id="forumMessage" class="collapse card card-body small">
-        <span>Hello!</span><br><br>
-        <span>You have been selected as a veto mediator for [url=https://osu.ppy.sh/beatmapsets/{{ selectedVeto.beatmapId }}]{{ selectedVeto.beatmapTitle }}[/url].</span><br>
-        <span>The map is currently vetoed for the following reason:</span><br>
-        <span>[notice] {{ selectedVeto.shortReason }} [/notice]</span><br>
-        <span>Veto discussion can be found [url={{ selectedVeto.discussionLink }}]here[/url].</span><br>
-        <span>Please post your opinion regarding the veto on the [url=http://bn.mappersguild.com/vetoes]BN/NAT website[/url] in [b]one week[/b]. Your decision will be anonymous to everyone but members of the NAT.</span><br>
-        <span>If you have a good reason not to participate in veto mediations, reply to this message.</span><br>
-        <span>Thank you for your hard work!</span>
+        <div>Hello!</div><br>
+        <div>You have been selected as a veto mediator for [url=https://osu.ppy.sh/beatmapsets/{{ selectedVeto.beatmapId }}]{{ selectedVeto.beatmapTitle }}[/url].</div>
+        <div>The map is currently vetoed for the following reason{{ selectedVeto.reasons.length > 1 ? 's' : '' }}:</div>
+        <div>
+            [notice][list]
+            <div v-for="reason in selectedVeto.reasons" :key="reason">
+                [*][url={{ reason.link }}]{{ reason.summary }}[/url]
+            </div>
+            [/list][/notice]
+        </div>
+        <div>Please post your opinion regarding the veto on the [url=http://bn.mappersguild.com/vetoes]BN/NAT website[/url] in [b]one week[/b]. Your decision will be anonymous to everyone but members of the NAT.</div>
+        <div>If you have a good reason not to participate in veto mediations, reply to this message.</div><br>
+        <div>Thank you for your hard work!</div>
     </div>
 </template>
 
