aabaa3eba8f2ca5c3ecda4ce8756c302dc9edbd2
pishifat <pishifat@gmail.com>
2021-05-02 18:27:21 -0700

    truncate previousevaluations (and dont show images/youtube)

--- src/components/evaluations/info/common/PreviousEvaluationEntry.vue
+++ src/components/evaluations/info/common/PreviousEvaluationEntry.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <router-link :to="'/evalarchive?id=' + evaluation.id" class="small">
-            {{ evaluation.archivedAt ? evaluation.archivedAt.slice(0,10) : evaluation.deadline.slice(0,10) }}
+            {{ evaluation.archivedAt ? evaluation.archivedAt.toString().slice(0,10) : evaluation.deadline.toString().slice(0,10) }}
             -
             <mode-display
                 :modes="evaluation.mode"
@@ -19,7 +19,17 @@
             </span>
         </small>
 
-        <div v-if="evaluation.feedback" v-html="$md.render(evaluation.feedback)" />
+        <div v-if="evaluation.feedback">
+            <div v-if="showFull || evaluation.feedback.length < 250" v-html="$md.render(evaluation.feedback)" />
+            <div v-else class="row">
+                <div class="col-sm-10" v-html="$md.render(short)" />
+                <div class="col-sm-2">
+                    <button class="btn btn-sm btn-primary" @click="showFull = true">
+                        Show full evaluation
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -39,12 +49,29 @@ export default {
             required: true,
         },
     },
+    data () {
+        return {
+            showFull: false,
+        };
+    },
     computed: {
         consensus () {
             return this.evaluation.consensus;
         },
         addition () {
             return this.evaluation.addition;
+        },
+        short () {
+            let i = this.evaluation.feedback.indexOf('![]');
+            let j = this.evaluation.feedback.indexOf('@[youtube]');
+
+            if (i == -1 || (i > j && j !== -1)) i = j;
+
+            if (i == -1 || i > 250) i = 250;
+
+            console.log(j);
+
+            return this.evaluation.feedback.slice(0,i).trim() + '... (*truncated*)';
         },
     },
 };
