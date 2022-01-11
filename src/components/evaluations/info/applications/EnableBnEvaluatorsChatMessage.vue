e8b3e5ede253a274806fded1ff9275b3580b06cd
pishifat <pishifat@gmail.com>
2022-01-11 15:51:51 -0800

    fix link

--- src/components/evaluations/info/applications/EnableBnEvaluatorsChatMessage.vue
+++ src/components/evaluations/info/applications/EnableBnEvaluatorsChatMessage.vue
@@ -36,7 +36,7 @@ export default {
             let messages = [];
 
             messages.push(`hello! you've been selected to help evaluate the ${this.selectedEvaluation.mode == 'osu' ? 'osu!' : 'osu!' + this.selectedEvaluation.mode} mode BN app for ${this.selectedEvaluation.user.username} - https://osu.ppy.sh/users/${this.selectedEvaluation.user.osuId}`);
-            messages.push(`please post your thoughts on their behavior and modding (based on the submitted mods and anything else you know) on the BN/NAT website - https://bn.mappersguild.com/appeval. your input is anonymous to everyone but the NAT!`);
+            messages.push(`please post your thoughts on their behavior and modding (based on the submitted mods and anything else you know) on the BN/NAT website - https://bn.mappersguild.com/appeval -- your input is anonymous to everyone but the NAT!`);
             messages.push(`if you can't see ${this.selectedEvaluation.user.username}'s eval, it already reached a consensus (this usually happens ~1 week after receiving this message!)`);
             messages.push(`evaluating users is an optional thing. if you'd like to opt out, visit your card on the users page of the BN website - https://bn.mappersguild.com/users`);
             messages.push(`thank you!!!! —NAT`);
