a20691c751fbd7f22893f58ef8edf6f37bd0742a
pishifat <31551350+pishifat@users.noreply.github.com>
2023-03-05 15:36:01 -0800

    hide 0/0

--- src/components/evaluations/info/currentBns/userActivity/NominationResets.vue
+++ src/components/evaluations/info/currentBns/userActivity/NominationResets.vue
@@ -4,14 +4,14 @@
             <a :href="events && `#${eventsId}`" data-toggle="collapse"
                 >{{ header }} <i class="fas fa-angle-down"
             /></a>
-            ({{ isLoading ? '...' : events ? events.length : '0' }})
+            ({{ isLoading ? '...' : events ? filteredEvents.length : '0' }})
         </div>
         <div v-if="events" :id="eventsId" class="collapse">
             <data-table
-                v-if="events.length"
+                v-if="filteredEvents.length"
                 :headers="['Date', 'Mapset', 'Reason']"
             >
-                <tr v-for="event in events" :key="event._id">
+                <tr v-for="event in filteredEvents" :key="event._id">
                     <td class="text-nowrap">
                         {{ event.timestamp | toMonthDay }}
                     </td>
@@ -38,6 +38,18 @@
                 </tr>
             </data-table>
             <p v-else class="small ml-4">None...</p>
+            <div 
+                v-if="events && events.length && (eventsId == 'nominationsDisqualified' || eventsId == 'nominationsPopped')"
+                class="text-secondary small"
+            >
+                <div>This list {{ filter ? 'excludes' : 'includes' }} <a href="https://osu.ppy.sh/wiki/en/People/The_Team/Nomination_Assessment_Team/SEV_rating" target="_blank">insignificant resets (0/0)</a>.</div>
+                <button
+                    class="btn btn-sm btn-primary mb-2"
+                    @click="filter = !filter"
+                >
+                    {{ `${filter == true ? 'Show' : 'Hide'} insignificant disqualifications/resets` }}
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -71,6 +83,28 @@ export default {
             required: true,
         },
     },
-    computed: mapState('activity', ['isLoading']),
+    data () {
+        return {
+            filter: this.eventsId == 'nominationsDisqualified' || this.eventsId == 'nominationsPopped',
+        };
+    },
+    computed: {
+        ...mapState('activity', ['isLoading']),
+        filteredEvents () {
+            let events = [...this.events];
+
+            if (this.filter) {
+                events = events.filter(e => {
+                    if (e.obviousness == 0 && e.severity == 0) {
+                        return false;
+                    }
+
+                    return true;
+                });
+            }
+
+            return events;
+        },
+    },
 };
 </script>
