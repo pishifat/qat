8225257d7e932f113f1339556e4dc744a46b2386
Hivie <rayenattia@gmail.com>
2023-05-26 00:25:28 +0100

    better handling of isReviewed requirement

--- src/components/evaluations/info/common/FeedbackInfo.vue
+++ src/components/evaluations/info/common/FeedbackInfo.vue
@@ -26,8 +26,7 @@
             v-if="
                 (
                     selectedEvaluation.feedback &&
-                    selectedEvaluation.feedback.length &&
-                    selectedEvaluation.isReviewed
+                    selectedEvaluation.feedback.length 
                 ) ||
                 selectedEvaluation.isResignation
             "
