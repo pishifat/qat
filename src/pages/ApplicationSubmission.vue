97701d1352a0cfe3780e09d2ede67a2599f53545
pishifat <31551350+pishifat@users.noreply.github.com>
2023-08-18 09:40:30 -0700

    remove default mode

--- src/pages/ApplicationSubmission.vue
+++ src/pages/ApplicationSubmission.vue
@@ -298,7 +298,7 @@ export default {
             cooldownResignations: [],
             relevantResignation: null,
             cooldown: false,
-            selectedMode: 'osu',
+            selectedMode: '',
             mods: [],
             reasons: [],
             oszs: [],
