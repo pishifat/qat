8c928f20aba883fba9ad08e14f1ce53875579677
Hivie <rayenattia@gmail.com>
2023-10-09 22:53:05 +0100

    add usersHome store

---
+++ src/store/usersHome.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+
+export default {
+    namespaced: true,
+    state: {
+        users: [],
+        selectedUserId: null,
+    },
+    mutations: {
+        setUsers (state, users) {
+            state.users = users;
+        },
+        setSelectedUserId (state, id) {
+            state.selectedUserId = id;
+        },
+
+        // modify data
+        updateUser (state, user) {
+            const i = state.users.findIndex(u => u.id === user.id);
+            if (i !== -1) Vue.set(state.users, i, user);
+        },
+    },
+    getters: {
+        selectedUser: (state) => {
+            return state.users.find(u => u.id === state.selectedUserId);
+        },
+    },
+};
