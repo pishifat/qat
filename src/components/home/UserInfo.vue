1dd3cd1adf4f13e21daade6e494d86ef50f5b61b
Hivie <rayenattia@gmail.com>
2023-10-11 10:31:37 +0100

    fix permalinks not working

--- src/components/home/UserInfo.vue
+++ src/components/home/UserInfo.vue
@@ -4,88 +4,90 @@
             <modal-header />
         </template>
 
-        <div class="ml-3">
-            <b>Status:</b>
-                <span
-                    class="badge badge-pill mx-1 text-lowercase"
-                    :class="getStatus(selectedUser.requestStatus) === 'closed' ? 'badge-danger' : getStatus(selectedUser.requestStatus) === 'open' ? 'badge-success' : 'badge-secondary'"
-                >
-                    {{ getStatus(selectedUser.requestStatus) }}
-                </span>
-        </div>
-        <div class="ml-3">
-            <b>Request methods:</b>
-                <span
-                    v-for="method in requestMethods(selectedUser.requestStatus)"
-                    :key="method"
-                    class="badge badge-pill mx-1 text-lowercase"
-                    :class="method === 'unknown' ? 'badge-secondary' : 'badge-primary'"
-                    v-html="$md.renderInline(formatLink(method, selectedUser.requestLink, selectedUser.osuId))"
-                />
-        </div>
-        <div v-if="selectedUser.languages.length" class="ml-3">
-            <b>Languages:</b>
-                <span
-                    v-for="language in selectedUser.languages"
-                    :key="language"
-                    class="language-tag badge badge-pill mx-1 text-lowercase badge-secondary"
-                    v-html="language"
-                />
-        </div>
-
-        <hr />
-
-        <div class="ml-3">
-            <div>
-                <b>Request information:</b>
-                <a
-                    v-if="loggedInUser._id === selectedUser.id && !isEditing"
-                    href="#"
-                    class="ml-1"
-                    @click.prevent="isEditing = !isEditing"
-                >
-                    <i class="fas fa-edit" />
-                </a>
-            </div>
-            <div v-if="isEditing">
-                <textarea
-                    v-model="requestInfo"
-                    class="form-control form-control-sm my-2"
-                    type="text"
-                    rows="4"
-                    placeholder="BN requests info"
-                />
-                
-                <button type="submit" class="btn btn-sm btn-secondary" @click="updateRequestInfo($event)">
-                    Update
-                </button>
-                <button type="submit" class="btn btn-sm btn-primary mx-2" @click="isEditing = !isEditing">
-                    Cancel
-                </button>
-                
-                
-            </div>
-            <div v-else>
-                <div v-if="selectedUser.requestInfo" class="card card-body small my-2">
-                    <span v-html="$md.render(selectedUser.requestInfo)" />
-                </div>
-                <span v-else class="small text-secondary">None...</span>
-            </div>
-        </div>
-
-        <hr />
-
-        <preferences />
-
-        <div v-if="loggedInUser.isPishifat">
-            <hr />
+        <div v-if="selectedUser">
             <div class="ml-3">
-                <b>Debug</b>
-                <div class="ml-3">
-                    <a href="#userDocument" data-toggle="collapse">
-                        view user document <i class="fas fa-angle-down" />
+                <b>Status:</b>
+                    <span
+                        class="badge badge-pill mx-1 text-lowercase"
+                        :class="getStatus(selectedUser.requestStatus) === 'closed' ? 'badge-danger' : getStatus(selectedUser.requestStatus) === 'open' ? 'badge-success' : 'badge-secondary'"
+                    >
+                        {{ getStatus(selectedUser.requestStatus) }}
+                    </span>
+            </div>
+            <div class="ml-3">
+                <b>Request methods:</b>
+                    <span
+                        v-for="method in requestMethods(selectedUser.requestStatus)"
+                        :key="method"
+                        class="badge badge-pill mx-1 text-lowercase"
+                        :class="method === 'unknown' ? 'badge-secondary' : 'badge-primary'"
+                        v-html="$md.renderInline(formatLink(method, selectedUser.requestLink, selectedUser.osuId))"
+                    />
+            </div>
+            <div v-if="selectedUser.languages.length" class="ml-3">
+                <b>Languages:</b>
+                    <span
+                        v-for="language in selectedUser.languages"
+                        :key="language"
+                        class="language-tag badge badge-pill mx-1 text-lowercase badge-secondary"
+                        v-html="language"
+                    />
+            </div>
+
+            <hr />
+
+            <div class="ml-3">
+                <div>
+                    <b>Request information:</b>
+                    <a
+                        v-if="loggedInUser._id === selectedUser.id && !isEditing"
+                        href="#"
+                        class="ml-1"
+                        @click.prevent="isEditing = !isEditing"
+                    >
+                        <i class="fas fa-edit" />
                     </a>
-                    <pre id="userDocument" class="collapse container text-white">{{ JSON.stringify(selectedUser, null, 4) }}</pre>
+                </div>
+                <div v-if="isEditing">
+                    <textarea
+                        v-model="requestInfo"
+                        class="form-control form-control-sm my-2"
+                        type="text"
+                        rows="4"
+                        placeholder="BN requests info"
+                    />
+                    
+                    <button type="submit" class="btn btn-sm btn-secondary" @click="updateRequestInfo($event)">
+                        Update
+                    </button>
+                    <button type="submit" class="btn btn-sm btn-primary mx-2" @click="isEditing = !isEditing">
+                        Cancel
+                    </button>
+                    
+                    
+                </div>
+                <div v-else>
+                    <div v-if="selectedUser.requestInfo" class="card card-body small my-2">
+                        <span v-html="$md.render(selectedUser.requestInfo)" />
+                    </div>
+                    <span v-else class="small text-secondary">None...</span>
+                </div>
+            </div>
+
+            <hr />
+
+            <preferences />
+
+            <div v-if="loggedInUser.isPishifat">
+                <hr />
+                <div class="ml-3">
+                    <b>Debug</b>
+                    <div class="ml-3">
+                        <a href="#userDocument" data-toggle="collapse">
+                            view user document <i class="fas fa-angle-down" />
+                        </a>
+                        <pre id="userDocument" class="collapse container text-white">{{ JSON.stringify(selectedUser, null, 4) }}</pre>
+                    </div>
                 </div>
             </div>
         </div>
@@ -155,7 +157,7 @@ export default {
         },
         /** @returns {string} */
         getStatus(requestStatus) {
-            if (!requestStatus.length) return 'unknown';
+            if (!requestStatus || !requestStatus.length) return 'unknown';
 
             if (requestStatus.includes('closed')) {
                 return 'closed';
