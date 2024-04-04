91c75202f63085165029910371940ab11c679b71
Hiviexd <rayenattia@gmail.com>
2024-04-04 21:36:50 +0100

    safer check

--- src/components/evaluations/info/common/ReviewContent.vue
+++ src/components/evaluations/info/common/ReviewContent.vue
@@ -2,7 +2,7 @@
     <div class="row my-3">
         <div class="col-sm-2">
             <user-avatar
-                v-if="loggedInUser.isNat || loggedInUser.isTrialNat"
+                v-if="(loggedInUser.isNat || loggedInUser.isTrialNat) && review.evaluator.username"
                 :user="review.evaluator"
                 :text-color="voteColor(review.vote)"
                 :align-start="true"
