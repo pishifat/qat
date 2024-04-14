21da547de3ae1327fc2b8047981d87219a9bba1f
Hiviexd <rayenattia@gmail.com>
2024-04-14 12:12:24 +0100

    block discord cdn from osz submission

--- routes/bnApp.js
+++ routes/bnApp.js
@@ -104,6 +104,18 @@ router.post('/apply', async (req, res) => {
                 error: `Must to write something in the ".osz" field for each mod`,
             });
         }
+
+        const blockedUrls = [
+            'puu.sh',
+            'cdn.discordapp.com'
+        ];
+
+        if (blockedUrls.some(url => oszs[i].includes(url))) {
+            const url = new URL(oszs[i]).hostname;
+            return res.json({
+                error: `Files hosted on ${url} are not allowed as they can expire quickly. Please use a different file host.`
+            });
+        }
     }
 
     // begin checks
@@ -404,7 +416,7 @@ router.post('/rejoinApply', async (req, res) => {
     await discord.webhookPost(
         [{
             author: discord.defaultWebhookAuthor(req.session),
-            description: `Requested to re-join ${mode == 'osu' ? 'osu!' : 'osu!' + mode} BN after recent resignation. See [BN application](http://bn.mappersguild.com/appeval?id=${newBnApp.id})`,
+            description: `Requested to re-join ${util.formatMode(mode)} BN after recent resignation. See [BN application](http://bn.mappersguild.com/appeval?id=${newBnApp.id})`,
             color: discord.webhookColors.lightYellow,
             fields,
         }],
@@ -418,7 +430,7 @@ router.post('/rejoinApply', async (req, res) => {
     // security check webhook
     const embed = [{
         author: discord.defaultWebhookAuthor(req.session),
-        description: `Requested to re-join ${mode == 'osu' ? 'osu!' : 'osu!' + mode} BN after recent resignation.`,
+        description: `Requested to re-join ${util.formatMode(mode)} BN after recent resignation.`,
         color: discord.webhookColors.lightYellow,
         fields,
     }];
