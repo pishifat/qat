e255d523b32ea01c6e4f95c6134ebbb93fab26c2
Hiviexd <rayenattia@gmail.com>
2024-04-22 19:25:21 +0100

    enable markdown in app comments

--- src/components/evaluations/info/applications/ApplicantComment.vue
+++ src/components/evaluations/info/applications/ApplicantComment.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="mb-2">
         <b>Comment:</b>
-        <span class="text-secondary">{{ comment || 'None...' }}</span>
+        <span v-if="!comment.length" class="text-secondary">None...</span>
+        <div v-else class="small card card-body v-html-content" v-html="$md.render(comment)" />
     </div>
 </template>
 
