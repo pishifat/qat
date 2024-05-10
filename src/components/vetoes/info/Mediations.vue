df6742cb4438e6d8e569f35e454513f78224c9a5
Hiviexd <rayenattia@gmail.com>
2024-05-10 01:24:53 +0100

    collapse mediation section
    list got too big with the inclusion of all bns so this makes browsing the page better

--- src/components/vetoes/info/Mediations.vue
+++ src/components/vetoes/info/Mediations.vue
@@ -1,16 +1,25 @@
 <template>
     <div>
+        <b>Mediations:</b>
         <div v-if="selectedVeto.vetoFormat >= 2">
             <div v-for="(reason, reasonIndex) in selectedVeto.reasons" :key="reasonIndex">
-                <a v-if="selectedVeto.reasons.length > 1" :href="reason.link" target="_blank">{{ reasonIndex+1 }}. {{ reason.summary }}</a>
-                <span v-for="(mediation, i) in selectedVeto.mediations" :key="mediation.id">
-                    <div v-if="mediation.reasonIndex == reasonIndex">
-                        <mediation-response
-                            :mediation="mediation"
-                            :i="i"
-                        />
-                    </div>
-                </span>
+                <a v-if="selectedVeto.reasons.length > 1" :href="'#reason-' + (reasonIndex + 1)" data-toggle="collapse">{{ reasonIndex + 1 }}. {{ reason.summary }} <i class="fas fa-angle-down" /></a>
+                <a v-else :href="'#reason-' + (reasonIndex + 1)" data-toggle="collapse">View mediations <i class="fas fa-angle-down" /></a>
+                <div :id="'reason-' + (reasonIndex + 1)" class="collapse">
+                    <span v-if="selectedVeto.reasons.length > 1">
+                        <hr />
+                        <b>Discussion link:</b>
+                        <a :href="reason.link" target="_blank">{{ reason.link }}</a>
+                    </span>
+                    <span v-for="(mediation, i) in selectedVeto.mediations" :key="mediation.id">
+                        <div v-if="mediation.reasonIndex == reasonIndex">
+                            <mediation-response
+                                :mediation="mediation"
+                                :i="i"
+                            />
+                        </div>
+                    </span>
+                </div>
             </div>
         </div>
         <div v-else>
