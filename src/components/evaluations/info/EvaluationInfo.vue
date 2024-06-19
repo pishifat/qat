e34da1936998fc30f46e758ccd302336ed313ae0
pishifat <31551350+pishifat@users.noreply.github.com>
2024-06-19 14:58:43 -0700

    maybe this will work

--- src/components/evaluations/info/EvaluationInfo.vue
+++ src/components/evaluations/info/EvaluationInfo.vue
@@ -119,7 +119,7 @@
 
                 <template v-if="selectedEvaluation.active">
                     <hr />
-                    <evaluation-input v-if="selectedEvaluation.isApplication || (!selectedEvaluation.user.evaluatorModes.includes(selectedEvaluation.mode) && !selectedEvaluation.user.modes.includes('none'))" />
+                    <evaluation-input v-if="selectedEvaluation.isApplication || (!selectedEvaluation.user.evaluatorModes.includes(selectedEvaluation.mode) && !selectedEvaluation.user.modes.includes('none') && !loggedInUser.isNatLeader)" />
                     <nat-self-evaluation v-else-if="!selectedEvaluation.discussion && selectedEvaluation.user.id == loggedInUser.id && (selectedEvaluation.user.evaluatorModes.includes(selectedEvaluation.mode) || selectedEvaluation.user.modes.includes('none'))" />
                     <nat-leader-evaluation v-else-if="selectedEvaluation.discussion && loggedInUser.isNatLeader && (selectedEvaluation.user.evaluatorModes.includes(selectedEvaluation.mode) || selectedEvaluation.user.modes.includes('none'))" />
                     <div v-else>
