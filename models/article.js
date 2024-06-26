b17deac61f3c16ab94f9beb64c4a7bbfa9575982
Hiviexd <rayenattia@gmail.com>
2024-06-26 19:48:10 +0100

    article model

---
+++ models/article.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+    type: { type: String, default: 'documentation', enum: ['documentation'] }, // enum for when/if we want to reuse articles
+    title: { type: String },
+    content: { type: String },
+}, { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } });
+
+class ArticleService extends mongoose.Model {
+    get slug() {
+        return this.title.replace(/ /g, '_');
+    }
+
+    static findByTitle(title) {
+        const parsedTitle = title.toLowerCase().replace(/_/g, ' ');
+        console.log('parsedTitle', parsedTitle);
+        return Article.findOne({
+            type: 'documentation',
+            $or: [
+                { title: new RegExp(`^${parsedTitle}$`, 'i') }, // case-insensitive
+            ]
+        });
+    }
+}
+
+articleSchema.loadClass(ArticleService);
+
+/**
+ * @type {import('./interfaces/article').IArticleModel}
+ */
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
