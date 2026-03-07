0a3c272c4f59a53a50357877de37126f9683ce41
Hivie <me@hivie.tn>
2026-03-07 17:30:13 +0100

    disable mongo sanitize

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(mongoSanitize());
+// app.use(mongoSanitize());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //natdb
