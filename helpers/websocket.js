c2fbf26e9c746555b7370aa22c9a5298fd55ec97
Hivie <me@hivie.tn>
2026-03-07 01:08:20 +0100

    add log on ws notification

--- helpers/websocket.js
+++ helpers/websocket.js
@@ -1,23 +1,21 @@
-/**
- * tags:
- * * users:request_status_update
- */
-
 class WebsocketManager {
-  static availableTags = ["users:request_status_update", "data:content_review"];
+    static sendNotification(type, data) {
+        console.log(`Sending notification for tag '${type}' to ${global.ws.length} websocket client(s)`);
 
-  static sendNotification(type, data) {
-    for (const client of global.ws.filter((client) =>
-      client.tags.includes(type)
-    )) {
-      client.ws.send(
-        JSON.stringify({
-          type,
-          data,
-        })
-      );
+        for (const client of global.ws.filter((client) => client.tags.includes(type))) {
+            client.ws.send(
+                JSON.stringify({
+                    type,
+                    data,
+                })
+            );
+        }
     }
-  }
 }
-
+/**
+ * Available tags:
+ * - `users:request_status_update`
+ * - `data:content_review`
+ */
+WebsocketManager.availableTags = ['users:request_status_update', 'data:content_review'];
 module.exports.websocketManager = WebsocketManager;
