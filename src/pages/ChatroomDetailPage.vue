3f5e2a0858cdc78560527f084a2a6918da8f6335
Hivie <me@hivie.tn>
2026-04-08 00:16:24 +0100

    cleanup stupid stupidity

--- src/pages/ChatroomDetailPage.vue
+++ src/pages/ChatroomDetailPage.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="row">
+    <div class="row ">
         <div class="col-md-12">
+            <chatroom-panel
+                v-if="roomId && !error"
+                :room-id="roomId"
+                :back-to="backTo"
+            />
             <div v-if="loading" class="text-center py-5">
                 <div class="spinner-border" role="status" />
             </div>
-            <div v-else-if="error" class="alert alert-danger">
+            <div v-if="!loading && error" class="alert alert-danger">
                 <i class="fas fa-exclamation-triangle me-2" />
                 {{ error }}
-            </div>
-            <div v-else class="container">
-                <chatroom-panel
-                    :room-id="roomId"
-                    :back-to="backTo"
-                />
             </div>
         </div>
         <toast-messages />
@@ -20,8 +19,6 @@
 </template>
 
 <script>
-import Axios from 'axios';
-import { mapGetters } from 'vuex';
 import chatroomsModule from '../store/chatrooms';
 import ChatroomPanel from '../components/chatrooms/ChatroomPanel.vue';
 import ToastMessages from '../components/ToastMessages.vue';
@@ -40,41 +37,28 @@ export default {
             backTo: null,
         };
     },
-    computed: {
-        ...mapGetters('chatrooms', ['roomById']),
-    },
     beforeCreate() {
         if (!this.$store.hasModule('chatrooms')) {
             this.$store.registerModule('chatrooms', chatroomsModule);
         }
     },
     async mounted() {
-        const roomId = this.$route.params.id;
-        this.roomId = roomId;
-
-        if (!roomId) {
+        const id = this.$route.params.id;
+        if (!id) {
             this.loading = false;
             this.error = 'Chatroom not found';
             return;
         }
-
-        try {
-            const { data } = await Axios.get(`/api/v2/chatrooms/${roomId}`);
-
-            if (!data.chatroom) {
-                this.error = data.error || 'Chatroom not found';
-                return;
-            }
-
+        this.roomId = id;
+        const data = await this.$http.initialRequest(`/v2/chatrooms/${id}`);
+        this.loading = false;
+        if (this.$http.isValid(data) && data.chatroom) {
             this.$store.commit('chatrooms/setRoom', data.chatroom);
-
             if (data.chatroom.type === 'veto' && data.chatroom.targetId) {
                 this.backTo = `/vetoes/${data.chatroom.targetId}`;
             }
-        } catch (error) {
-            this.error = 'Chatroom not found';
-        } finally {
-            this.loading = false;
+        } else {
+            this.error = data?.error || 'Chatroom not found';
         }
     },
 };
